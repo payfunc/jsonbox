7aa7d019429837c9fd06f5494b85bcb371ad61a2
Florian Ebmeier <florian.ebmeier@payfunc.com>
2020-09-03 11:40:19 +0200

    Changed fetchResponse input  to remove undefined

--- Collection.ts
+++ Collection.ts
@@ -2,6 +2,13 @@ import * as gracely from "gracely"
 import { fetch, Response as FetchResponse } from "./fetch"
 import { Document } from "./Document"
 
+export interface parameterInterface {
+	method: string
+	jsonId?: string
+	bodyResource?: Record<string, unknown>
+	metaData?: boolean
+}
+
 export class Collection<T> {
 	private readonly headers: { [key: string]: string }
 	constructor(private readonly url: string = "https://jsonbox.io", private readonly boxId: string) {
@@ -10,12 +17,12 @@ export class Collection<T> {
 			Accept: "application/json; charset=utf-8",
 		}
 	}
-
-	private async fetchResponse(method: string, bodyResource?: T, id?: string, metaData?: boolean) {
-		const body = JSON.stringify(bodyResource, undefined, "  ")
-		const url = this.url + (metaData ? "/_meta/" : "/") + this.boxId + (id == undefined ? "" : "/" + id)
+	private async fetchResponse(input: { method: string; jsonId?: string; bodyResource?: any; metaData?: boolean }) {
+		const body = JSON.stringify(input.bodyResource, undefined, "  ")
+		const url =
+			this.url + (input.metaData ? "/_meta/" : "/") + this.boxId + (input.jsonId == undefined ? "" : "/" + input.jsonId)
 		const response = await fetch(url, {
-			method: method,
+			method: input.method,
 			headers: this.headers,
 			body,
 		})
@@ -23,23 +30,23 @@ export class Collection<T> {
 	}
 
 	async create(resource: T): Promise<Document<T> | gracely.Error> {
-		return this.fetchResponse("POST", resource, undefined)
+		return this.fetchResponse({ method: "POST", bodyResource: resource })
 	}
 
 	async list(): Promise<Document<T>[] | gracely.Error> {
-		return this.fetchResponse("GET", undefined, undefined)
+		return this.fetchResponse({ method: "GET" })
 	}
 	async get(id: string): Promise<Document<T> | gracely.Error> {
-		return this.fetchResponse("GET", undefined, id)
+		return this.fetchResponse({ method: "GET", jsonId: id })
 	}
 	async delete(id: string): Promise<true | gracely.Error> {
-		return this.fetchResponse("DELETE", undefined, id)
+		return this.fetchResponse({ method: "DELETE", jsonId: id })
 	}
 	async update(resource: T, id: string): Promise<true | gracely.Error> {
-		return this.fetchResponse("PUT", resource, id)
+		return this.fetchResponse({ method: "PUT", jsonId: id, bodyResource: resource })
 	}
 	async getMetadata(): Promise<T | gracely.Error> {
-		return this.fetchResponse("PUT", undefined, undefined, true)
+		return this.fetchResponse({ method: "PUT", metaData: true })
 	}
 
 	private async getError(response: FetchResponse): Promise<gracely.Error> {
